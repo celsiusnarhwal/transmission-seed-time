f859bfbb1efdeae033a98e5ec383a9f57d8d25ca
celsius narhwal <hello@celsiusnarhwal.dev>
2024-08-25 15:40:20 -0400

    Add mod files

---
+++ root/etc/s6-overlay/s6-rc.d/init-mod-transmission-seed-time-postinstall/run.py
@@ -0,0 +1,7 @@
+#!/usr/bin/with-contenv python
+
+import json
+
+transmission_settings = json.load(open("/config/settings.json"))
+transmission_settings["idle-seeding-limit-enabled"] = False
+json.dump(transmission_settings, open("/config/settings.json", "w"), indent=2)
---
+++ root/etc/s6-overlay/s6-rc.d/svc-mod-transmission-seed-time/run.py
@@ -0,0 +1,34 @@
+#!/usr/bin/with-contenv python
+
+import time
+from datetime import datetime
+
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from transmission_rpc import Client, IdleMode
+
+
+class ModSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="TR_")
+
+    minimum_seeding_time: int = Field(0, ge=0)
+    idle_seeding_limit: int = Field(0, ge=0)
+
+
+settings = ModSettings()
+tc = Client()
+
+while True:
+    for torrent in tc.get_torrents():
+        minutes_since_done = (
+            datetime.now().astimezone() - torrent.done_date
+        ).total_seconds() / 60
+
+        if minutes_since_done >= settings.minimum_seeding_time:
+            tc.change_torrent(
+                torrent.info_hash,
+                seed_idle_limit=settings.idle_seeding_limit,
+                seed_idle_mode=IdleMode.Single,
+            )
+
+    time.sleep(5)
